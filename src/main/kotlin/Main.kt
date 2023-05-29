0bbfc1bc17ef37ac5cb8ead4989f2cd376d914b7
Vlad Gromov <vladoffrevan@mail.ru>
2023-05-29 21:59:27 +0300

    change repository

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+import attachments.*
+import posts.Comments
+import posts.Likes
+import posts.Post
+import posts.PostType
+import java.util.*
+
+fun main() {
+    WallService.add(Post(ownerId = 1, text = "14120"))
+    //Thread.sleep(1000)
+    WallService.add(Post(ownerId = 1, text = "132249"))
+    WallService.add(Post(ownerId = 2, text = "141303"))
+    WallService.add(Post(ownerId = 2, text = "402010", postType = PostType.REPLY))
+
+    WallService.add(Post(ownerId = 3, text = "222222", likes = Likes(canLike = true)))
+
+    WallService.update(Post(id = 2, text = "replaced", comments = Comments(canClose = true)))
+    WallService.update(Post(id = 1, text = "new text", comments = Comments(canOpen = true), canPin = false))
+
+    WallService.add(Post(attachments = arrayOf(AudioAttachment(), EventAttachment(), NoteAttachment(), PostedPhotoAttachment(),
+        GraffitiAttachment())))
+
+    println(WallService)
+}
